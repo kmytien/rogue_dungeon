c6422f870fc6ae73c92299bf57e7ba14992bcfc0
Haylee Lawrence <hlawrence8121@gmail.com>
2021-02-07 15:34:17 -0600

    Update 327_assignment1.01.c

--- 327_assignment1.01.c
+++ 327_assignment1.01.c
@@ -4,6 +4,8 @@
 #include <stdbool.h>
 #include <unistd.h>
 
+
+
 /** RULES FOR THE DUNGEON MAP
 • Dungeon measures 80 units x direction and 21 units y direction
 • at least 6 rooms per dungeon
@@ -11,14 +13,11 @@
 • Rooms need not be rectangular, but neither may they contact one another. There must be at least 1
 cell of non-room between any two different rooms.
 • The outermost cells of the dungeon are immutable, thus they must remain rock and cannot be part of
-any room or corridor.
-• There should be at least one up and one down staircase. Staircases work like floor (for now) and
-should be placed in a location where floor would otherwise be. A character on the floor somewhere in
-the dungeon should be able to walk to the stairs.
-• Room cells should be drawn with periods, corridor cells with hashes, rock with spaces, up staircases
-with less-than signs, and down staircases with greater-than signs.
-• The dungeon should be fully connected, meaning that from any position on the floor, your adventurer
-should be able to walk to any other position on the floor without passing through rock.
+any room or corridor. ***(THE BORDER????) -HL
+• At least one down and one up stairfase (> and < respectively). Placed in a location where floor would be and must have at least one side open to access it
+• Room (.), corridor cells (#), rock ( ), up staircases (<), and down staircases (>)
+• The dungeon should be fully connected, meaning that from any position on the floor, your adventurer should be able to walk to any other position 
+on the floor without passing through rock.
 • Corridors should not extend into rooms, e.g., no hashes should be rendered inside rooms.
 **/
 
@@ -29,5 +28,8 @@ int main(int argc, char* argv[]) {
 // print function
 
 // random generator function
+// BRAINSTORMING??
+// WE COULD MAKE A RANDOM NUM GENERATOR THAT GOES TO RANDOM CELLS AND CAN CHOOSE TO DO A CERTAIN TYPE OF CELL IF THERE ARE SIMILAR CELLS NEARBY, ELSE IT MAKES A CORRIDOR OR 
+// SOMETHING?????????? -HL
 
 
