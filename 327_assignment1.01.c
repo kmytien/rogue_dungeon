d06c8b882506b7edc42994448f2913386a64471a
Haylee Lawrence <hlawrence8121@gmail.com>
2021-02-07 15:26:17 -0600

    Update 327_assignment1.01.c

--- 327_assignment1.01.c
+++ 327_assignment1.01.c
@@ -4,5 +4,30 @@
 #include <stdbool.h>
 #include <unistd.h>
 
+/** RULES FOR THE DUNGEON MAP
+• Dungeon measures 80 units x direction and 21 units y direction
+• at least 6 rooms per dungeon
+• Each room measures at least 4 units in the x direction and at least 3 units in the y direction.
+• Rooms need not be rectangular, but neither may they contact one another. There must be at least 1
+cell of non-room between any two different rooms.
+• The outermost cells of the dungeon are immutable, thus they must remain rock and cannot be part of
+any room or corridor.
+• There should be at least one up and one down staircase. Staircases work like floor (for now) and
+should be placed in a location where floor would otherwise be. A character on the floor somewhere in
+the dungeon should be able to walk to the stairs.
+• Room cells should be drawn with periods, corridor cells with hashes, rock with spaces, up staircases
+with less-than signs, and down staircases with greater-than signs.
+• The dungeon should be fully connected, meaning that from any position on the floor, your adventurer
+should be able to walk to any other position on the floor without passing through rock.
+• Corridors should not extend into rooms, e.g., no hashes should be rendered inside rooms.
+**/
+
+// main function to execute the dungeon creator
 int main(int argc, char* argv[]) {
 }
+
+// print function
+
+// random generator function
+
+
