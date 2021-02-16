1b1a721f2188b7cb4a3bc1e6bbc632a0c38486f3
Haylee Lawrence <hlawrence8121@gmail.com>
2021-02-16 11:45:46 -0600

    updates to main based off of lecture

--- 02_loadSave/327_assignment1.02.c
+++ 02_loadSave/327_assignment1.02.c
@@ -45,8 +45,7 @@ int main(int argc, char* argv[]) {
     char *savefile = "dungeon";
     char *filepath = malloc(strlen(directory) + strlen(gameDir) + strlen(savefile) + 2 + 1);
     mkdir(directory, S_IRWXU);
-    
-    // sprintf(filepath, "%s/&s/%s", home, gameDir, savefile);
+    sprintf(filepath, "%s/&s/%s", home, gameDir, savefile);
 
     file = fopen(directory, "wb+");
 
