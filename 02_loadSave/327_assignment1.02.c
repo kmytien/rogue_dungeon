b6963edcdc6b93e30065f1039b29b27819b5053d
Haylee Lawrence <hlawrence8121@gmail.com>
2021-02-17 17:49:05 -0600

    Update 327_assignment1.02.c

--- 02_loadSave/327_assignment1.02.c
+++ 02_loadSave/327_assignment1.02.c
@@ -6,11 +6,8 @@
 #include <endian.h>
 #include "rlg327.c"
 
-
 void saveDungeon(char*);
 void loadDungeon(char*);
-
-dungeon_t *dungeon;
 
 /**
  * CS 327: Assignment 1.02: Dungeon Load/Save
@@ -35,8 +32,7 @@ dungeon_t *dungeon;
 **/
 
 int main(int argc, char* argv[]) {
-    // make the .directory in home and set the directory name for reference
-
+    //make the .directory in home and set the directory name for reference
     char *directory = getenv("HOME");
     char *gameDir = ".rlg327";
     char *savefile = "dungeon";
@@ -44,12 +40,7 @@ int main(int argc, char* argv[]) {
 
     mkdir(directory, S_IRWXU);
     sprintf(filepath, "%s/%s/%s", directory, gameDir, savefile);
-    fopen(filepath, "w");
-
-    if(filepath == NULL) {
-        fprintf(stderr, "ERROR, Could not open dungeon file");
-        exit(1);
-    }
+    //fopen(filepath, "w"); - we dont have to open file yet
 
     if (strcmp("--save", argv[1]) == 0)
         saveDungeon(filepath);
@@ -64,121 +55,166 @@ int main(int argc, char* argv[]) {
 
     else {
         //generates and prints dungeon like normal
-        gen_dungeon(&*dungeon);
+        initDungeon();
+        createRooms();
+        printDungeon();
     }
 }
 
-//fseek - moves file pointer position to certain location (wherever you put in parenthesis)
-//SEEK_SET - moves file pointer position to the beginning of the file
-//fread - reads data into an array
 
 //load-- read a dungeon from the file and print it
 void loadDungeon(char* filepath) {
     int row, col, rooms;
     FILE* f = fopen(filepath, "r");
+
+    if (f == NULL) {
+        fprintf(stderr, "ERROR, Could not open dungeon file");
+        exit(1);
+    }
+
     //read semantic file type marker
     fseek(f, 0, SEEK_SET);
     char file_type[13];
     file_type[12] = '\0';
-    fread(file_type, 1, 12, f);
+    fread(file_type, sizeof(char), 12, f);
 
     //read file version
-    fseek(f, 4, SEEK_SET); //(its like a fancy 32 bit integer)
-    uint32_t version;
-    uint32_t read_value;
-    fread(&read_value, sizeof(uint32_t), 1, f);
-    version = be32toh(read_value);
+    fseek(f, 12, SEEK_SET);
+    uint32_t version = 0;
+    fread(&version, sizeof(uint32_t), 1, f);
+    version = be32toh(version);
 
     //read the size of the file
-    fseek(f, 4, SEEK_SET);
-    int size;
-    int be_size;
-    fread(&be_size, sizeof(uint32_t), 1, f);
-    size = be32toh(be_size);
+    fseek(f, 16, SEEK_SET);
+    uint32_t size;
+    fread(&size, sizeof(uint32_t), 1, f);
+    size = be32toh(size);
 
-    //would print characters of dungeon based on hardness
-    for (row = 0; row < 21; row++) {
-      for (col = 0; col < 80; col++) {
-        //if hardness is 255 outermost cells
-        if (dungeon.hardness[row][col] -> 255) {
-          dungeon[row][col] = ter_wall_immutable;
+    //pc - do we have to convert to big endian here? -- check
+    fseek(f, 20, SEEK_SET);
+    uint8_t pc_x;
+    uint8_t pc_y;
+    fread(&pc_x, sizeof(uint8_t), 1, f);
+    fread(&pc_y, sizeof(uint8_t), 1, f);
+
+    //hardness values
+    fseek(f, 22, SEEK_SET);
+    int8_t h[21][80]; //do we even need this
+
+    for(int i = 0; i < 21; i++) {
+        for(int j = 0; j < 80; j++) {
+            //or is it &hardness[i][j]?? -- check
+            fread(&h, 1, 1680, f);
         }
-        //if hardness 0 - corridors/rooms
-        else if (dungeon.hardness == 0) {
-          dungeon[row][col] = ter_floor; // HOW DO I FIND THE HARDNESS OF STAIRS
+    }
+
+    for (int i = 0; i < 21; i++) {
+        for (int j = 0; j < 80; j++) {
+            if (hardness[i][j] > 0 && hardness[i][j] < 255)
+                dungeon[i][j] = ' ';
+
+            else if (hardness[i][j] == 0)
+                dungeon[i][j] = '#';
+
+            else if (hardness[i][j] == 255) {
+                if (i > 18 || i < 2 || j > 77 || j < 2)
+                    dungeon[i][j] = '@'; // prob have to change a few things e.o - bc @ represents player and here o, representing it as immutable hardness
+            }
         }
-        else if(dungeon.hardness > 1 && dungeon.hardness < 255) {
-          dungeon[row][col] = ter_wall;
+    }
+
+    //reads rooms
+    fseek(f, 1702, SEEK_SET);
+    int count = malloc(size - 1704);
+    Room rooms = count / 4;
+    fread(count, 1, (size - 1704), f);
+
+    int i, j = 0;
+    for(i = 0; i < (size - 1704)/4; i++) {
+        int x, y, w, h; // w and h are width and height
+
+        //look into struct in 1.01
+        rooms[i].xstart = count[j];
+        rooms[i].ystart = count[j + 1];
+        rooms[i].w = count[j + 2];
+        rooms[i].h = count[j + 3];
+
+        /*fread(&x, sizeof(int8_t), 1, f);
+        fread(&y, sizeof(int8_t), 1, f);
+        fread(&w, sizeof(int8_t), 1, f);
+        fread(&h, sizeof(int8_t), 1, f);*/
+    }
+
+    // plot rooms in dungeon based on read from files
+    int o, p, q;
+    for(o = 0; i < (size - 1704)/4; o++) {
+      for(p = rooms[o].y; p < rooms[o].y + rooms[o].h; p++) {
+        for(q = rooms[o].x; q < rooms[o].x + rooms[o].w; q++) {
+          dungeon[p][q] = '.';
         }
       }
     }
-    // add rooms
-    // HOW DO I ADD STAIRCASES
-    for (rooms = 0; rooms < dungeon.num_rooms; rooms++) {
-      for (row = room.position[dim_y]; row < room.size[dim_y]; row++) {
-        for (col = room.position[dim_x]; col < room.size[dim_x]; col++) {
-          dungeon[row][col] = ter_floor_room;
-        }
-      }
-    }
-    //print dungeon
-    render_dungeon(dungeon);
 
-    // hardness values? and whatever they have to do with files
-    int i, j;
-    for(i = 0; i < 21; i++) {
-      for(j = 0; j < 80; j++) {
-        int8_t h[21][80];
-        fread(&h, 1, 1680, f);
-      }
-    }
+    //stairs
 
-
-
-
+    printDungeon();
     fclose(f);
 }
 
-//save dungeon to a file in the directory
+//save dungeon to a file in the directory (write)
 void saveDungeon(char* filepath) {
 
+    FILE* f = fopen(filepath, "w");  //filepathpath      pathpath    pathpath      filepathpath
 
-    FILE* f = fopen(filpath, "w");  //filepathpath      pathpath    pathpath      filepathpath
-
-    //semantic file_type marker
-    char semantic[13] = "RLG327-S2021";
-    fwrite(&semantic, 1, 12, f);
-
-    //file version marker set to 0
-    fseek(f, 0, SEEK_SET);
-    uint32_t marker = 0;
-    marker = be32toh(marker);
-    fwrite(&marker, 1, 4, f);
-
-    //size of files
-    uint32_t size;
-    size = be32toh(size);
-    fwrite(&size, 1, 4, f);
-
-    //PC
-    uint8_t PCx;
-    uint8_t PCy;
-    fwrite(&PCx, 1, 1, f);
-    fwrite(&PCy, 1, 1, f);
-
-    //hardness - do we have to do something more here?
-    uint8_t hardness[21][80];
-    for (int i = 0; i < 21; i++) {
-      for (int j = 0; j < 80; j++) {
-          fwrite(&hardness, 1, 1680, f);
-      }
+    if (f == NULL) {
+        fprintf(stderr, "ERROR, Could not open dungeon file");
+        exit(1);
     }
 
-    //rooms
-    uint16_t rooms;
+    //file_type
+    fseek(f, 0, SEEK_SET);
+    char file_type[13] = "RLG327-S2021";
+    fwrite(&file_type, sizeof(char), 12, f);
+    file_type = be32oh(file_type);
+
+    //file version marker set to 0
+    fseek(f, 12, SEEK_SET);
+    uint32_t version = 0;
+    fwrite(&version, sizeof(uint32_t), 1, f); //is it 1 or is it 4??
+    version = be32toh(version);
+
+    //size of files
+    fseek(f, 16, SEEK_SET);
+    uint32_t size;
+    fwrite(&size, sizeof(uint32_t), 1, f);
+    size = be32toh(size);
+
+    //PC
+    fseek(f, 20, SEEK_SET);
+    uint8_t pc_x;
+    uint8_t pc_y;
+    fwrite(&pc_x, sizeof(uint8_t), 1, f);
+    fwrite(&pc_y, sizeof(uint8_t), 1, f);
+
+    //hardness values
+    fseek(f, 22, SEEK_SET);
+    int8_t h[21][80]; //do we even need this
+
+    for(int i = 0; i < 21; i++) {
+        for(int j = 0; j < 80; j++) {
+            //or is it &hardness[i][j]?? -- check
+            fread(&h, 1, 1680, f);
+        }
+    }
+
+    //writes rooms
+    // GLOBAL? VARIABLE IN 1.01 IS tot_rooms I THINK WE CAN ACCESS IT W/O ISSUE
+    uint8_t rooms[tot_rooms][4];
+    // for loop to write the corrdinates x and y then the x size (width) and then y length (height)
+    fwrite(&rooms, 1, 4*tot_rooms, f);
+
 
     //close file
     fclose(f);
 
 }
-
