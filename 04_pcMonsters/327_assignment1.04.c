7c87ba3398cc2937d8b77a1ecf29ca9538587e26
mytien kien! <55749308+kmytien@users.noreply.github.com>
2021-03-01 15:14:05 -0600

    Update 327_assignment1.04.c

--- 04_pcMonsters/327_assignment1.04.c
+++ 04_pcMonsters/327_assignment1.04.c
@@ -1,1 +1,29 @@
+//include statements below
 
+
+/**
+    Notes:
+      *** may interpret these however we like ***
+      
+      - Need to move PC around
+        - Represented with '@'
+        - Can place PC in any open space
+        
+      - Need to move monsters around
+        - "--nummon [INTEGER]" summons an INTEGER number of monsters around (10 is reasonable)
+        
+        - Represented with letters and occasionally letters/punctuation
+        
+        - Monsters may have 50% chance of having the following:
+          - Intelligence: understands dungeon layout and moves on shortest path, can remember last known position
+            of previously seen PC
+          - Unintelligent: monsters move in a straight line toward PC
+          
+          - Telepathy: always know where PC is, moves toward PC
+          - Non-Telepathic: knows where PC is if PC is in line of sight, moves toward last known position
+          
+          - Tunneling Ability: can tunnel through rock
+          
+          - Erratic Behavior: have a 50% chance of moving as per their other characteristics,
+          otherwise they move to a random neighboring cell
+**/
