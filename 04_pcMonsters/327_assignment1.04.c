97c37ac5a61646ed84efb92be31c12274f8f4f06
mytien kien! <55749308+kmytien@users.noreply.github.com>
2021-03-01 15:16:36 -0600

    Update 327_assignment1.04.c

--- 04_pcMonsters/327_assignment1.04.c
+++ 04_pcMonsters/327_assignment1.04.c
@@ -11,19 +11,21 @@
         
       - Need to move monsters around
         - "--nummon [INTEGER]" summons an INTEGER number of monsters around (10 is reasonable)
-        
         - Represented with letters and occasionally letters/punctuation
-        
         - Monsters may have 50% chance of having the following:
-          - Intelligence: understands dungeon layout and moves on shortest path, can remember last known position
-            of previously seen PC
-          - Unintelligent: monsters move in a straight line toward PC
+              - Intelligence: understands dungeon layout and moves on shortest path, can remember last known position
+                of previously seen PC
+              - Unintelligent: monsters move in a straight line toward PC
+
+              - Telepathy: always know where PC is, moves toward PC
+              - Non-Telepathic: knows where PC is if PC is in line of sight, moves toward last known position
+
+              - Tunneling Ability: can tunnel through rock
+
+              - Erratic Behavior: have a 50% chance of moving as per their other characteristics,
+              otherwise they move to a random neighboring cell
           
-          - Telepathy: always know where PC is, moves toward PC
-          - Non-Telepathic: knows where PC is if PC is in line of sight, moves toward last known position
-          
-          - Tunneling Ability: can tunnel through rock
-          
-          - Erratic Behavior: have a 50% chance of moving as per their other characteristics,
-          otherwise they move to a random neighboring cell
+      - Speed
+        - Each monsters gets speed between 5 and 20
+        - PC gets speed of 10
 **/
