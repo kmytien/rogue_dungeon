9e6b42d39c9fb3509640bf43bb36385e3f73a211
Haylee Lawrence <hlawrence8121@gmail.com>
2021-03-09 16:49:16 -0600

    Update dungeon.c

--- 04_pcMonsters/dungeon.c
+++ 04_pcMonsters/dungeon.c
@@ -621,49 +621,49 @@ int gen_dungeon(dungeon_t *d)
     return 0;
 }
 
-void render_dungeon(dungeon_t *d)
-{
-    pair_t p;
-    int num;
+// void render_dungeon(dungeon_t *d)
+// {
+//     pair_t p;
+//     int num;
 
-    for (p[dim_y] = 0; p[dim_y] < DUNGEON_Y; p[dim_y]++) {
-        for (p[dim_x] = 0; p[dim_x] < DUNGEON_X; p[dim_x]++) {
-            if (d->pc.position[dim_x] == p[dim_x] && d->pc.position[dim_y] == p[dim_y]) {
-                putchar('@');
-            } else if (d->mons[p[dim_y]][p[dim_x]] != 0) {
-                num = (int) d->mons[p[dim_y]][p[dim_x]];
-                putchar(d->monsters[num].m);
-            } else {
-                switch (mappair(p)) {
-                    case ter_wall:
-                    case ter_wall_immutable:
-                        putchar(' ');
-                        break;
-                    case ter_floor:
-                    case ter_floor_room:
-                        putchar('.');
-                        break;
-                    case ter_floor_hall:
-                        putchar('#');
-                        break;
-                    case ter_debug:
-                        putchar('*');
-                        fprintf(stderr, "Debug character at %d, %d\n", p[dim_y], p[dim_x]);
-                        break;
-                    case ter_stairs_up:
-                        putchar('<');
-                        break;
-                    case ter_stairs_down:
-                        putchar('>');
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-        putchar('\n');
-    }
-}
+//     for (p[dim_y] = 0; p[dim_y] < DUNGEON_Y; p[dim_y]++) {
+//         for (p[dim_x] = 0; p[dim_x] < DUNGEON_X; p[dim_x]++) {
+//             if (d->pc.position[dim_x] == p[dim_x] && d->pc.position[dim_y] == p[dim_y]) {
+//                 putchar('@');
+//             } else if (d->mons[p[dim_y]][p[dim_x]] != 0) {
+//                 num = (int) d->mons[p[dim_y]][p[dim_x]];
+//                 putchar(d->monsters[num].m);
+//             } else {
+//                 switch (mappair(p)) {
+//                     case ter_wall:
+//                     case ter_wall_immutable:
+//                         putchar(' ');
+//                         break;
+//                     case ter_floor:
+//                     case ter_floor_room:
+//                         putchar('.');
+//                         break;
+//                     case ter_floor_hall:
+//                         putchar('#');
+//                         break;
+//                     case ter_debug:
+//                         putchar('*');
+//                         fprintf(stderr, "Debug character at %d, %d\n", p[dim_y], p[dim_x]);
+//                         break;
+//                     case ter_stairs_up:
+//                         putchar('<');
+//                         break;
+//                     case ter_stairs_down:
+//                         putchar('>');
+//                         break;
+//                     default:
+//                         break;
+//                 }
+//             }
+//         }
+//         putchar('\n');
+//     }
+// }
 
 void delete_dungeon(dungeon_t *d)
 {
