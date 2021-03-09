2e7668cbff72d691746fbf72ee6954f3bfc9efe0
mytien kien! <55749308+kmytien@users.noreply.github.com>
2021-03-09 16:54:37 -0600

    Created 1.05 Header file

---
+++ 05_NcursesUI/assignment_105.h
@@ -0,0 +1,20 @@
+#ifndef ASSIGNMENT_105_H
+#define ASSIGNMENT_105_H
+
+#include "dungeon.h"
+
+int pc_commands(dungeon_t *d, int key);
+
+int stairs(dungeon_t *d);
+
+uint32_t pc_next_pos(dungeon_t *d, pair_t dir);
+
+void create_monster_list(dungeon_t *d);
+
+void scroll_monster_list(dungeon_t *d, uint32_t count);
+
+void display_monster_list(dungeon_t *d, character_t **monsters);
+
+void display_render_dungeon(dungeon_t *d);
+
+#endif
