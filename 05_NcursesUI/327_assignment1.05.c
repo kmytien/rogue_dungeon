45aac9733631fa608fa815350e1186948e066791
mytien kien! <55749308+kmytien@users.noreply.github.com>
2021-03-09 20:57:46 -0600

    Update 327_assignment1.05.c

--- 05_NcursesUI/327_assignment1.05.c
+++ 05_NcursesUI/327_assignment1.05.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
+#include <unistd.h>
 #include <ncurses.h>
+#include <ctype.h>
+#include <stdlib.h>
 
-#include "dungeon.h"
-#include "pc.h"
-#include "utils.h"
 #include "move.h"
 #include "path.h"
-#include "npc.h"
-#include "character.h"
+#include "pc.h"
+#include "utils.h"
+#include "dungeon.h"
+#include "assignment_105.h"
 
 
 /**
@@ -36,7 +37,7 @@
 
 
 //feel free to change the name for this, not really sure if this is needed, its just me trying to figure out ncurses
-int pc_commands(dungeon_t *d, int key) {
+int pc_commands(dungeon_t *d) {
     //getch(); pauses to wait for an input, have to press enter i think?
     //printw(...); prints stuff to the screen
     //refresh(); updates the screen
@@ -44,124 +45,129 @@ int pc_commands(dungeon_t *d, int key) {
     pair_t dir;
     char input = getch();
     uint32_t escape_key;
+    bool cont = true;
+    //do {
+    //  if((key == getch()) == 27) {
+    //      display_render_dungeon(d);
+    //      return;
+    //  }
+    //} while();
+    while (cont) {
+        switch(input) {
 
-    do{
-      if((key == getch()) == 27) {
-          display_render_dungeon(d);
-          return;
-      }
-    }
+            //move pc to upper left?
+            case '7':
+            case 'y':
+            case KEY_HOME:
+                dir[dim_y] = -1;
+                dir[dim_x] = -1;
+                pc_next_pos(d, dir);
+                break;
 
-    switch(input) {
+            //move pc one cell up
+            case '8':
+            case 'k':
+            case KEY_UP:
+                dir[dim_y] = -1;
+                dir[dim_x] = 0;
+                pc_next_pos(d, dir);
+                break;
 
-        //move pc to upper left?
-        case '7':
-        case 'y':
-        case KEY_HOME:
-            dir[dim_y] = -1;
-            dir[dim_x] = -1;
-            pc_next_pos(d, dir);
-            break;
+            //move pc to upper right
+            case '9':
+            case 'u':
+            case KEY_PPAGE:
+                dir[dim_y] = -1;
+                dir[dim_x] = 1;
+                pc_next_pos(d, dir);
+                break;
 
-        //move pc one cell up
-        case '8':
-        case 'k':
-        case KEY_UP:
-            dir[dim_y] = -1;
-            dir[dim_x] = 0;
-            pc_next_pos(d, dir);
-            break;
+            //move pc one cell to right
+            case '6':
+            case 'l':
+            case KEY_RIGHT:
+                dir[dim_y] = 0;
+                dir[dim_x] = 1;
+                pc_next_pos(d, dir);
+                break;
 
-        //move pc to upper right
-        case '9':
-        case 'u':
-        case KEY_PPAGE:
-            dir[dim_y] = -1;
-            dir[dim_x] = 1;
-            pc_next_pos(d, dir);
-            break;
+            //move pc one cell to lower right
+            case '3':
+            case 'n':
+            case KEY_NPAGE:
+                dir[dim_y] = 1;
+                dir[dim_x] = 1;
+                pc_next_pos(d, dir);
+                break;
 
-        //move pc one cell to right
-        case '6':
-        case 'l':
-        case KEY_RIGHT:
-            dir[dim_y] = 0;
-            dir[dim_x] = 1;
-            pc_next_pos(d, dir);
-            break;
+            //move pc one cell DOWN
+            case '2':
+            case 'j':
+            case KEY_DOWN:
+                dir[dim_y] = 1;
+                dir[dim_x] = 0;
+                pc_next_pos(d, dir);
+                break;
 
-        //move pc one cell to lower right
-        case '3':
-        case 'n':
-        case KEY_NPAGE:
-            dir[dim_y] = 1;
-            dir[dim_x] = 1;
-            pc_next_pos(d, dir);
-            break;
+            //move pc one cell to lower left
+            case '1':
+            case 'b':
+            case KEY_END:
+                dir[dim_y] = 1;
+                dir[dim_x] = -1;
+                pc_next_pos(d, dir);
+                break;
 
-        //move pc one cell DOWN
-        case '2':
-        case 'j':
-        case KEY_DOWN:
-            dir[dim_y] = 1;
-            dir[dim_x] = 0;
-            pc_next_pos(d, dir);
-            break;
+            //move pc one cell to the left
+            case '4':
+            case 'h':
+            case KEY_LEFT:
+                dir[dim_y] = 0;
+                dir[dim_x] = -1;
+                pc_next_pos(d, dir);
+                break;
 
-        //move pc one cell to lower left
-        case '1':
-        case 'b':
-        case KEY_END:
-            dir[dim_y] = 1;
-            dir[dim_x] = -1;
-            pc_next_pos(d, dir);
-            break;
+            //attempt to go down stairs. Works only if standing on down staircase.
+            case '>':
+            //if pc is going downstairs
+                stairs(d);
+                break;
 
-        //move pc one cell to the left
-        case '4':
-        case 'h':
-        case KEY_LEFT:
-            dir[dim_y] = 0;
-            dir[dim_x] = -1;
-            pc_next_pos(d, dir);
-            break;
+            //attempt to go up stairs. Works only if standing on up staircase.
+            case '<':
+            //if pc is going upstairs
+                stairs(d);
+                break;
 
-        //attempt to go down stairs. Works only if standing on down staircase.
-        case '>':
-        //if pc is going downstairs
-            stairs(d);
-            break;
+            //npcs still move, x and y = 0
+            case '5':
+            case ' ':
+            case '.':
+            case KEY_B2:
+                dir[dim_y] = 0;
+                dir[dim_x] = 0;
+                pc_next_pos(&d, dir);
+                cont = false;
+                break;
 
-        //attempt to go up stairs. Works only if standing on up staircase.
-        case '<':
-        //if pc is going upstairs
-            stairs(d);
-            break;
+            case 'm':
+                create_monster_list(d);
+                cont = false;
+                break;
 
-        //npcs still move, x and y = 0
-        case '5':
-        case ' ':
-        case KEY_B2:
-            dir[dim_y] = 0;
-            dir[dim_x] = 0;
-            pc_next_pos(&d, dir);
-            break;
+            case 'Q':
+                //delete pc
+                pc_delete(d->pc.pc);
 
-        case 'm':
-          create_monster_list(d);
+                //delete dungeon
+                delete_dungeon(d);
+                endwin(); //close stdscr
+                abort();
+                break; //we put break here right? I'm not sure we need a break cause of abort
 
-        case 'Q':
-            //delete pc
-            pc_delete(d->pc.pc);
-
-            //delete dungeon
-            delete_dungeon(d);
-            endwin(); //close stdscr
-            abort();
-            break; //we put break here right? I'm not sure we need a break cause of abort
-
-        default:
-            return 0;
+            default:
+                return 0;
+        }
     }
 }
 
@@ -170,16 +176,18 @@ int stairs(dungeon_t *d) {
     //delete pc
     pc_delete(d->pc.pc); // I think thats how you get pc from current dungeon
     //delete current dungeon
-    delete_dungeon(&d);
+    delete_dungeon(d);
     //generate new dungeon
     init_dungeon(&d);
     gen_dungeon(&d);
     //generate new pc
     config_pc(&d);
+    d->character[d->pc.position[dim_y]][d->pc.position[dim_x]] = &d->pc;
     //generate gen_monsters
     gen_monsters(&d);
+    
     //renders dungeon
-    render_dungeon(d);
+    //display_render_dungeon(&d);
 }
 
 
@@ -188,12 +196,16 @@ uint32_t pc_next_pos(dungeon_t *d, pair_t dir) {
   static uint32_t have_seen_corner = 0;
   static uint32_t count = 0;
 
-  while(!pc_commands(&d, dir)) { // this is gonna be an issue --probably :'(
+  while(!pc_commands(d)) { // this is gonna be an issue --probably :'(
     return 0;
   }
 
-  d->pc.position[dim_x] = dir[dim_x];
-  d->pc.position[dim_y] = dir[dim_y];
+  pair_t next_pos;
+  d->pc.position[dim_x] += dir[dim_x];
+  d->pc.position[dim_y] += dir[dim_y];
+  next_pos[dim_y] = d->pc.position[dim_y];
+  next_pos[dim_x] = d->pc.position[dim_x];
+  move_character(d, &d->pc, next_pos);
 
   return 0;
 }
@@ -202,20 +214,21 @@ uint32_t pc_next_pos(dungeon_t *d, pair_t dir) {
 //creating a list of all the monsters
 void create_monster_list(dungeon_t *d) {
 
-    character_t *monsters;
+    character_t **monsters;
     uint32_t slot = 0;
     monsters = malloc(d->num_monsters * sizeof(*monsters));
 
     for (int i = 0; i < DUNGEON_Y; i++) {
-        for (int j = 0; i < DUNGEON_X; j++) {
-            if (d->character[i][j] != d->pc && d->character[i][j] != NULL) {
+        for (int j = 0; j < DUNGEON_X; j++) {
+            if (d->character[i][j] != &d->pc && d->character[i][j] != NULL) {
                 monsters[slot] = d->character[i][j];
                 slot++;
             }
         }
     }
 
-    display_monster_list(d, monster, slot);
+    display_monster_list(d, monsters);
+    display_render_dungeon(d);
     free(monsters);
 }
 
@@ -224,30 +237,38 @@ void create_monster_list(dungeon_t *d) {
 void scroll_monster_list(dungeon_t *d, uint32_t count){
 
     uint32_t num = 0;
+    bool foo = true;
 
-    switch(getch()) {
-        case KEY_UP:
-            if (num) num--;
-            break;
+    while (foo) {
+    	//for (i = 0; i < 13; i++) {
+        //    mvprintw(i + 6, 19, " %-40s ", s[i + offset]);
+        //}
+    
+        switch(getch()) {
+            case KEY_UP:
+                if (num) num--;
+                break;
 
-        case KEY_DOWN:
-            if (num < (count - 10)) num++;
-            break;
+            case KEY_DOWN:
+                if (num < (count - 10)) num++;
+                break;
 
-        case 27:
-          return;
+            case 27:
+              foo = false;
+              return;
+        }
     }
 }
 
 
 //io to display the list on terminal
 //Display a list of monsters in the dungeon, with their symbol and position relative to the PC (e.g.: “c, 2 north and 14 west”).
-void display_monster_list(dungeon_t *d, character_t *monsters){
+void display_monster_list(dungeon_t *d, character_t **monsters){
     char(*x)[40];
     uint32_t count = (d->num_monsters * sizeof(*monsters));
 
-    char north_south[6];
-    char west_east[5];
+    char *north_south;
+    char *west_east;
 
     x = malloc(count * sizeof(*x));
 
@@ -275,64 +296,78 @@ void display_monster_list(dungeon_t *d, character_t *monsters){
         else north_south = "SOUTH";
         NS_dir = pc_y - my;
 
-        //print ------------------------------------------------- idk how to do this
-        //mvprintw(x, y(index of where you want it to be on terminal), 40 char from x array , message or value you want to print);
-        mvprintw(i + 4, 19, "%-40s", x[i]);
+        mvprintw(i + 4, 19, "The monster %c is %d %s and %d %s",
+                 monsters[i]->symbol, NS_dir, north_south, WE_dir, west_east);
+
+        if (count < 11) mvprintw(i + 4, 19, "%-40s", x[i]);
     }
 
-    if (count <= 10) {
+    if (count < 11) {
         mvprintw(count + 4, 19, "%-40s", " ");
         mvprintw(count + 5, 19, "%-40s", "Press the escape button to continue.");
         //as long as user doesn't hit escape continue to else
-    } else {
+    }
+
+    else {
         mvprintw(18, 19, "%-40s", " ");
         mvprintw(19, 19, "%-40s", "Arrows allow you to scroll, press the escape button to continue.");
         scroll_monster_list(d, count);
     }
+
     free(x);
 }
 
 // new render dungeon that prints to the ncurse screen
-void display_render_dungeon(dungeon_t *d)
-{
-  pair_t p;
+void display_render_dungeon(dungeon_t *d) {
+    pair_t p;
+    clear();
 
-  putchar('\n');
-  for (p[dim_y] = 0; p[dim_y] < DUNGEON_Y; p[dim_y]++) {
-    for (p[dim_x] = 0; p[dim_x] < DUNGEON_X; p[dim_x]++) {
-      if (charpair(p)) {
-        mvaddch(charpair(p)->symbol);
-      } else {
-        switch (mappair(p)) {
-        case ter_wall:
-        case ter_wall_immutable:
-          mvaddch(p[dim_y] + 1, p[dim_x], ' ');
-          break;
-        case ter_floor:
-        case ter_floor_room:
-          mvaddch(p[dim_y] + 1, p[dim_x],'.');
-          break;
-        case ter_floor_hall:
-          mvaddch(p[dim_y] + 1, p[dim_x],'#');
-          break;
-        case ter_debug:
-          mvaddch(p[dim_y] + 1, p[dim_x],'*');
-          fprintf(stderr, "Debug character at %d, %d\n", p[dim_y], p[dim_x]);
-          break;
-        case ter_stairs_up:
-          mvaddch(p[dim_y] + 1, p[dim_x],'<');
-          break;
-        case ter_stairs_down:
-          mvaddch(p[dim_y] + 1, p[dim_x],'>');
-          break;
-        default:
-          mvaddch(p[dim_y] + 1, p[dim_x], '0');
+    putchar('\n');
+    for (p[dim_y] = 0; p[dim_y] < DUNGEON_Y; p[dim_y]++) {
+        for (p[dim_x] = 0; p[dim_x] < DUNGEON_X; p[dim_x]++) {
+            if (charpair(p)) {
+                mvaddch(p[dim_y] + 1, p[dim_x], charpair(p)->symbol); //this is where it stops printing..
+            }
+
+            else {
+                switch (mappair(p)) {
+
+                    case ter_wall:
+                    case ter_wall_immutable:
+                        mvaddch(p[dim_y] + 1, p[dim_x], ' ');
+                        break;
+
+                    case ter_floor:
+                    case ter_floor_room:
+                        mvaddch(p[dim_y] + 1, p[dim_x], '.');
+                        break;
+
+                    case ter_floor_hall:
+                        mvaddch(p[dim_y] + 1, p[dim_x], '#');
+                        break;
+
+                    case ter_debug:
+                        mvaddch(p[dim_y] + 1, p[dim_x], '*');
+                        fprintf(stderr, "Debug character at %d, %d\n", p[dim_y], p[dim_x]);
+                        break;
+
+                    case ter_stairs_up:
+                        mvaddch(p[dim_y] + 1, p[dim_x], '<');
+                        break;
+
+                    case ter_stairs_down:
+                        mvaddch(p[dim_y] + 1, p[dim_x], '>');
+                        break;
+
+                    default:
+                        mvaddch(p[dim_y] + 1, p[dim_x], ';');
+                }
+            }
         }
-      }
     }
-    mvaddch(p[dim_y] + 1, p[dim_x],'\n');
-  }
-  mvaddch('\n');
-  mvaddch('\n');
-  refresh();
+    
+    //printf("\n\nhey\n\n");
+    mvaddch(p[dim_y] + 1, p[dim_x], '\n');
+    mvaddch(p[dim_y] + 1, p[dim_x], '\n');
+    refresh();
 }
