20caa4e296c22296e32d0e45c7530ec9a4a5c3c6
mytien kien! <55749308+kmytien@users.noreply.github.com>
2021-03-10 17:24:25 -0600

    Update assignment_105.h

--- 05_NcursesUI/assignment_105.h
+++ 05_NcursesUI/assignment_105.h
@@ -13,7 +13,7 @@ void create_monster_list(dungeon_t *d);
 
 void scroll_monster_list(dungeon_t *d, uint32_t count);
 
-void display_monster_list(dungeon_t *d, character_t **monsters);
+void display_monster_list(dungeon_t *d, character_t **monsters, uint32_t slot);
 
 void display_render_dungeon(dungeon_t *d);
 
