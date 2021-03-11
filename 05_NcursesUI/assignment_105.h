f69c81871fd55d3d8bbe50362fa87f701a6405f6
mytien kien! <55749308+kmytien@users.noreply.github.com>
2021-03-11 10:58:20 -0600

    Update assignment_105.h

--- 05_NcursesUI/assignment_105.h
+++ 05_NcursesUI/assignment_105.h
@@ -6,16 +6,16 @@
 
 int stairs(dungeon_t *d);
 
-uint32_t pc_next_pos(dungeon_t *d, pair_t dir);
+uint32_t key_inputs(dungeon_t *d, pair_t dir);
 
 void create_monster_list(dungeon_t *d);
 
 void scroll_monster_list(dungeon_t *d, character_t **monsters, uint32_t count);
 
-void print_monster_list(dungeon_t *d, character_t **monsters, uint32_t begin, uint32_t end);
+void print_list(dungeon_t *d, character_t **monsters, uint32_t begin, uint32_t end);
 
 void display_monster_list (dungeon_t *d, character_t **monsters, uint32_t slot);
 
-void display_render_dungeon(dungeon_t *d);
+void display_nc_dungeon(dungeon_t *d);
 
 #endif
