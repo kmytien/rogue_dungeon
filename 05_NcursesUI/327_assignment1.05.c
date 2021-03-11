519253b0c0f7b7a6d9466dfb1f66dc032bfee850
mytien kien! <55749308+kmytien@users.noreply.github.com>
2021-03-10 18:37:14 -0600

    Update 327_assignment1.05.c

--- 05_NcursesUI/327_assignment1.05.c
+++ 05_NcursesUI/327_assignment1.05.c
@@ -12,47 +12,23 @@
 #include "npc.h"
 
 
-/**
-  NOTES:
-  - Make staircases functional
-    - when a player goes up or down stairs generate a new dungeon and new monsters (up <, down >) WE SHOULD DO NON PERSISTANT/YOU CANNOT GO BACK TO THE SAME LEVEL
-      - PC gets put in a random spot on the floor
-  - Set commands NEVER NEED TO PRESS ENTER, A PRESSED KEY ACTIVATES COMMAND IMMEDIATELY, PRINT MESSAGES IF A COMMAND IS NOT POSSIBLE
-    - <, > go up (<) or down (>) stairs if on a stair block ** NPCS CANNOT USE STAIRS
-    - 7, y move PC 1 cell to the upper left
-    - 8, k move PC 1 cell up
-    - 9, u move PC 1 cell to upper right
-    - 6, l move PC one cell to the right
-    - 3, n move PC one cell to lower right
-    - 2, j move PC one cell down
-    - 1, b move PC one cell to the lower right
-    - 4, h move PC one cell to the left
-    - 5, [SPACE], . Rest for turn, NPCs still move
-    - m display a list of monsters in the dungeon, with their symbol and position relative to the PC (e.g.: “c, 2 north and 14 west”).
-    - [UP ARROW] when displaying monster list, if entire list does not fit in screen and not currently at top of list, scroll list up
-    - [DOWN ARROW] when displaying monster list, if entire list does not fit in screen and not currently at bottom of list, scroll list down
-    - [ESC] when displaying monster list, return back to player control
-    - Q quit game
-   **DISPLAY IS LINES 1-21, USE 0 TO DISPLAY ERROR MESSAGES
-**/
-
-
 int stairs(dungeon_t *d) {
     //delete pc
-    pc_delete(d->pc.pc); // I think thats how you get pc from current dungeon
+    pc_delete(d->pc.pc); 
+    
     //delete current dungeon
     delete_dungeon(d);
+    
     //generate new dungeon
     init_dungeon(d);
     gen_dungeon(d);
+    
     //generate new pc
     config_pc(d);
     d->character[d->pc.position[dim_y]][d->pc.position[dim_x]] = &d->pc;
+    
     //generate gen_monsters
     gen_monsters(d);
-    
-    //renders dungeon
-    //display_render_dungeon(&d);
 }
 
 
@@ -62,12 +38,13 @@ uint32_t pc_next_pos(dungeon_t *d, pair_t dir) {
 
     mvprintw(0, 20, "%-40s", "Press a key to move the PC in a direction.");
 
-    int input = getch();
+    int input; 
     bool cont = false;
 
     while (!cont) {
-        switch (input) {
-            //move pc to upper left?
+        switch (input = getch()) {
+        
+            //move pc to upper left
             case '7':
             case 'y':
             case KEY_HOME:
@@ -178,13 +155,12 @@ uint32_t pc_next_pos(dungeon_t *d, pair_t dir) {
 
                 //delete dungeon
                 delete_dungeon(d);
-                endwin(); //close stdscr
+                endwin(); 
                 abort();
-                break; //we put break here right? I'm not sure we need a break cause of abort
+                break; 
             
             default:
                 mvprintw(0, 20, "%-40s", "	ERROR: Please press a valid key.	");
-                break;
         }
     }
 
@@ -228,9 +204,6 @@ void scroll_monster_list(dungeon_t *d, uint32_t count){
     bool foo = true;
 
     while (foo) {
-    	//for (i = 0; i < 13; i++) {
-        //    mvprintw(i + 6, 19, " %-40s ", s[i + offset]);
-        //}
     
         switch(getch()) {
             case KEY_UP:
@@ -244,10 +217,7 @@ void scroll_monster_list(dungeon_t *d, uint32_t count){
             case 27:
               foo = false;
               return;
-              break;
         }
-        
-        refresh();
     }
 }
 
@@ -255,18 +225,13 @@ void scroll_monster_list(dungeon_t *d, uint32_t count){
 //io to display the list on terminal
 //Display a list of monsters in the dungeon, with their symbol and position relative to the PC (e.g.: “c, 2 north and 14 west”).
 void display_monster_list(dungeon_t *d, character_t **monsters, uint32_t slot){
-    char(*x)[40];
     uint32_t count = slot;
 
     char *north_south;
     char *west_east;
-
-    x = malloc(count * sizeof(*x));
     
     mvprintw(1, 19, "%-40s", " ");
-    snprintf(x[0], 40, "You know of %d monsters:", count);
-
-    mvprintw(2, 19, "%-40s", x);
+    mvprintw(2, 19, "You know of %d monsters:		", count);
     mvprintw(3, 19, "%-40s", " ");
 
     uint32_t i;
@@ -275,6 +240,7 @@ void display_monster_list(dungeon_t *d, character_t **monsters, uint32_t slot){
     int NS_dir, WE_dir;
 
     for (i = 0; i < count; i++) {
+    
         //print how far away from pc the monster is
         mx = monsters[i]->position[dim_x];
         my = monsters[i]->position[dim_y];
@@ -290,9 +256,11 @@ void display_monster_list(dungeon_t *d, character_t **monsters, uint32_t slot){
         mvprintw(i + 4, 19, "The monster %c is %3d %s and %3d %s",
                  monsters[i]->symbol, NS_dir, north_south, WE_dir, west_east);
 
-        if (count < 11) mvprintw(i + 4, 19, "%-40s", x[i]);
+        //if (count < 11) mvprintw(i + 4, 19, "%-40s", x[i]);
     }
-
+	
+	
+	
     if (count < 11) {
         mvprintw(count + 4, 19, "%-40s", " ");
         mvprintw(count + 5, 19, "%-40s", "Press the escape button to continue.");
@@ -304,11 +272,10 @@ void display_monster_list(dungeon_t *d, character_t **monsters, uint32_t slot){
         mvprintw(19, 19, "%-40s", "Arrows to scroll & ESC to go back");
         scroll_monster_list(d, count);	    
     }
-
-    free(x);
 }
 
-// new render dungeon that prints to the ncurse screen
+//new render dungeon that prints to the ncurse screen
+//used sheaffer's render_dungeon and just changed putchar to mvaddch
 void display_render_dungeon(dungeon_t *d) {
     pair_t p;
     clear();
@@ -317,7 +284,7 @@ void display_render_dungeon(dungeon_t *d) {
     for (p[dim_y] = 0; p[dim_y] < DUNGEON_Y; p[dim_y]++) {
         for (p[dim_x] = 0; p[dim_x] < DUNGEON_X; p[dim_x]++) {
             if (charpair(p)) {
-                mvaddch(p[dim_y] + 1, p[dim_x], charpair(p)->symbol); //this is where it stops printing..
+                mvaddch(p[dim_y] + 1, p[dim_x], charpair(p)->symbol);
             }
 
             else {
@@ -357,7 +324,6 @@ void display_render_dungeon(dungeon_t *d) {
         }
     }
     
-    //printf("\n\nhey\n\n");
     mvaddch(p[dim_y] + 1, p[dim_x], '\n');
     mvaddch(p[dim_y] + 1, p[dim_x], '\n');
     refresh();
