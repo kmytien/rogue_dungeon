0117510090b81a5a9fbd197e740370e4100a1586
mytien kien! <55749308+kmytien@users.noreply.github.com>
2021-03-22 15:40:49 -0500

    Added displays for no/fog

--- 06_fogOfWar/io.h
+++ 06_fogOfWar/io.h
@@ -5,7 +5,8 @@ typedef struct dungeon dungeon_t;
 
 void io_init_terminal(void);
 void io_reset_terminal(void);
-void io_display(dungeon_t *d);
+void io_display_nf(dungeon_t *d);
+void io_display_f(dungeon_t *d);
 void io_handle_input(dungeon_t *d);
 void io_queue_message(const char *format, ...);
 
