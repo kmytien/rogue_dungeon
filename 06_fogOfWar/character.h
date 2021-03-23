330ee7f64bf5918a2def7b7519982352c62adf26
Haylee Lawrence <hlawrence8121@gmail.com>
2021-03-23 16:45:45 -0500

    made into a class

--- 06_fogOfWar/character.h
+++ 06_fogOfWar/character.h
@@ -5,18 +5,25 @@
 
 # include "dims.h"
 
-typedef struct dungeon dungeon_t;
-typedef struct npc npc_t;
-typedef struct pc pc_t;
+// typedef struct dungeon dungeon_t;
+// typedef struct npc npc_t;
+// typedef struct pc pc_t;
 typedef struct dice_t dice_t;
 
-typedef enum kill_type {
-  kill_direct,
-  kill_avenged,
-  num_kill_types
-} kill_type_t;
+// typedef enum kill_type {
+//   kill_direct,
+//   kill_avenged,
+//   num_kill_types
+// } kill_type_t;
 
-typedef struct character {
+class character_t {
+  public:
+  typedef enum kill_type {
+    kill_direct,
+    kill_avenged,
+    num_kill_types
+  } kill_type_t;
+
   char symbol;
   pair_t position;
   int32_t speed;
@@ -32,11 +39,12 @@ typedef struct character {
   npc_t *npc;
   pc_t *pc;
   uint32_t kills[num_kill_types];
-} character_t;
 
-int32_t compare_characters_by_next_turn(const void *character1,
-                                        const void *character2);
-uint32_t can_see(dungeon_t *d, character_t *voyeur, character_t *exhibitionist);
-void character_delete(void *c);
+  
+  int32_t compare_characters_by_next_turn(const void *character1, const void *character2);
+  uint32_t can_see(dungeon_t *d, character_t *voyeur, character_t *exhibitionist);
+  void character_delete(void *c);
+
+};
 
 #endif
