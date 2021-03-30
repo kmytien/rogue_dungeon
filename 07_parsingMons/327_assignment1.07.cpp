b6220433af81d06ec0a4f594384541341eafba4e
mytien kien! <55749308+kmytien@users.noreply.github.com>
2021-03-30 09:28:45 -0500

    Adding lots of comments

--- 07_parsingMons/327_assignment1.07.cpp
+++ 07_parsingMons/327_assignment1.07.cpp
@@ -11,10 +11,11 @@
   *	COMS 327: Assignment 1.07 - Parsing Monster Desc.
   * By: Sanjana Amatya, MyTien Kien, Haylee Lawrence
   *
-**/  
+**/
 
 using namespace std;
 
+//monster descriptions class
 class monster_desc {
 public:
     string name;
@@ -27,10 +28,11 @@ public:
     string symbol;
     string rrty;
 
+    //printing monsters
     void print_monsters(){
-        // PRINT THE MONSTERS
         cout << name << desc << symbol << color << speed << ability << hp << damage << rrty << endl;
 
+        //resetting monster description variables
         name.erase();
         desc.erase();
         color.erase();
@@ -44,152 +46,146 @@ public:
 };
 
 
+//reading from file and filling in the monster desc. variables
 int parse_monsters() {
 
-  string filename;
-  char c;
-  filename = getenv("HOME");
-  filename += "/.rlg327/monster_desc.txt"; //not sure if this is how you get stuff from another directory
+    string filename;
+    filename = getenv("HOME");
+    filename += "/.rlg327/monster_desc.txt";
 
-  ifstream mfile;
-  monster_desc md;
-  string s = "";
+    ifstream mfile;
+    monster_desc md;
+    string s = "";
+    char c;
 
-  mfile.open(filename);
+    //opening file, checks if file is opened
+    mfile.open(filename);
+    if (!mfile.is_open()) {
+        cout << "Cannot open file." << endl;
+        return -1;
+    }
 
-  if (!mfile.is_open()) {
-      cout << "Cannot open file." << endl;
-      return 1; // 1 is error code for, file didnt open
-  }
-
-  // READ RLG327 MONSTER DESCRIPTION 1 AND IF IT FAILS TO MATCH, EXIT WITH ERROR CODE -1 code has been tested :) -h
-
-  getline(mfile, s, '\n');
-	mfile.get(c);
-	
-  if (s != "RLG327 MONSTER DESCRIPTION 1") {
-      cout << "File is in wrong format." << endl;
-      return -1; 
-  }
-	
-	
-  //read the data
-  while (1) {
-  	
-    // BEGIN MONSTER
+    //checking for first line - "RLG327 MONSTER DESCRIPTION 1", then skipping new line
     getline(mfile, s, '\n');
-    cout << s << endl;
-    if (s != "BEGIN MONSTER") break; //HOPEFULLY THIS GOES TO THE NEXT BEGIN MONSTER??
-		c = mfile.peek();
-		
-	  while (c != 'E') {
-	  	
-	  	if (c == '\n' || c == '.') continue;
-	  	
-		  //NAME Junior Barbarian
-		  if (c == 'N') {
-			  getline(mfile, s, ' ');
-			  getline(mfile, s, '\n');
-			  md.name = s + "\n";
-	 	  }
-	 
-	 		//SYMBOL & SPEED
-	 	  else if (c == 'S') {
-	 	  	getline(mfile, s, ' ');
-	 	  	
-	 	  	if (s == "SYMB") { //symbol
-					getline(mfile, s, '\n');
-					md.symbol = s + "\n";
-			  }
-			  
-			  else if (s == "SPEED") { //speed
-					getline(mfile, s, '\n');
-					md.speed = s + "\n";
-			  }
-		  }
+  	mfile.get();
 
-		  //COLOR BLUE
-		  else if (c == 'C') {
-				getline(mfile, s, ' ');
-				getline(mfile, s, '\n');
-				md.color = s + "\n";
-			}
+    if (s != "RLG327 MONSTER DESCRIPTION 1") {
+        cout << "File is in wrong format." << endl;
+        return -1;
+    }
 
-		  //DESC
-		  else if (c == 'D') {
-		  	mfile.get(); //D is extracted
-		  	mfile.get(c); //whatever next letter is extracted
-		  	
-		  	if (c == 'E') { //desc
-					getline(mfile, s, '\n');
-					getline(mfile, s, '\n');
-					
-					while (s != ".") {
-						md.desc += s + "\n";
-						getline(mfile, s, '\n');
-					}
-				}
-				
-				else if (c == 'A') { //dam
-					getline(mfile, s, ' ');
-					getline(mfile, s, '\n');
-					md.damage = s + "\n";
-				}
-		  }
 
-		  //HP 12+2d6
-		  else if (c == 'H') {
-				getline(mfile, s, ' ');
-				getline(mfile, s, '\n');
-				md.hp = s + "\n";
-			}
+    //while we are not at the EOF
+    while (mfile) {
 
-		  //RRTY 100
-		  else if (c == 'R') {
-				getline(mfile, s, ' ');
-				getline(mfile, s, '\n');
-				md.rrty = s + "\n";
-		  }
-		  
-		  //ABIL SMART
-		  else if (c == 'A') {
-				getline(mfile, s, ' ');
-				getline(mfile, s, '\n');
-				md.ability = s + "\n";
-		  }
-		  
-		  c = mfile.peek();
-	  }
-		
-    //END
-    getline(mfile, s, '\n');
-    if (s != "END") break;
-    md.print_monsters();
-    
-    mfile.get();
-  }
+        //looking for "BEGIN MONSTER"
+        getline(mfile, s, '\n');
+        if (s != "BEGIN MONSTER") break;
 
-  mfile.close();
-  return 0;
+        //peeks at next beginning character
+    	c = mfile.peek();
+
+        //while the next beginnign character is NOT "E" - so if next word is NOT "END"
+    	while (c != 'E') {
+
+            //continues if next character is a newline or a period
+      	    if (c == '\n' || c == '.') continue;
+
+      		  //checking for name
+      		  if (c == 'N') {
+        	  	getline(mfile, s, ' ');
+        		getline(mfile, s, '\n');
+        		md.name = s + "\n";
+      	 	  }
+
+      	 	  //checking for symbol and speed
+      	 	  else if (c == 'S') {
+        	 	getline(mfile, s, ' ');
+
+                	//case for symbol
+        		 if (s == "SYMB") {
+          		        getline(mfile, s, '\n');
+          		        md.symbol = s + "\n";
+        		 }
+
+                	//case for speed
+        		else if (s == "SPEED") {
+          			getline(mfile, s, '\n');
+          			md.speed = s + "\n";
+        		}
+      		  }
+
+      		  //checking for color
+      		  else if (c == 'C') {
+        		getline(mfile, s, ' ');
+        		getline(mfile, s, '\n');
+        		md.color = s + "\n";
+      		  }
+
+      		  //checking for description or damage
+      		  else if (c == 'D') {
+        		mfile.get(); //D is extracted
+        		mfile.get(c); //whatever next letter is extracted
+
+                	//case for description
+        		if (c == 'E') {
+          		        getline(mfile, s, '\n');
+          	                getline(mfile, s, '\n');
+
+          		        while (s != ".") {
+          				md.desc += s + "\n";
+          				getline(mfile, s, '\n');
+          		        }
+        	  	}
+
+                	//case for damage
+        		else if (c == 'A') {
+          			getline(mfile, s, ' ');
+          			getline(mfile, s, '\n');
+          			md.damage = s + "\n";
+        		}
+      		  }
+
+      		  //checking for hp
+      		  else if (c == 'H') {
+        		getline(mfile, s, ' ');
+        		getline(mfile, s, '\n');
+        		md.hp = s + "\n";
+      		  }
+
+      		  //checking for rarity
+      		  else if (c == 'R') {
+        		getline(mfile, s, ' ');
+       			getline(mfile, s, '\n');
+        		md.rrty = s + "\n";
+      		  }
+
+      		  //checking for abilities - check if parses more than one word
+      		  else if (c == 'A') {
+        		getline(mfile, s, ' ');
+        		getline(mfile, s, '\n');
+        		md.ability = s + "\n";
+      		  }
+
+            	  //peeking at next letter in next line
+      		  c = mfile.peek();
+      	}
+
+        //checking for END
+        getline(mfile, s, '\n');
+        if (s != "END") break;
+        md.print_monsters();
+
+        //skips the newline before "BEGIN MONSTER" and starts over
+        mfile.get();
+        }
+
+    mfile.close();
+    return 0;
 }
 
+//main method
 int main(int argc, char *argv[]) {
     parse_monsters();
     return 0;
 }
-
-//  EXAMPLE FROM VIDEO ON PARSING
-//
-// while(mfile) {
-//   string line;
-//   getline(input, line, ':'); // from example where file said UK Pop: 64000000, reads till : and then throws the : away
-//
-//   int population;
-//   mfile >> population;
-//
-//   mfile.get(); // takes the newline character and throws it away
-//
-//   if(!input)  break; // ends the file read so that we dont print garbage
-//
-//   cout << line << " -- " << population << endl; // prints UK Pop -- 64000000
-// }                                              //         France Pop -- 664000000
