77118319d1f9da2e8a4eaabb4ccc27d229edb15a
Haylee Lawrence <hlawrence8121@gmail.com>
2021-04-06 19:27:30 -0500

    added the classes

--- 08_loadMonsObj/assignment_108.h
+++ 08_loadMonsObj/assignment_108.h
@@ -4,15 +4,28 @@
 #include "descriptions.h"
 #include "dungeon.h"
 
-class object{
+class object {
+public:
+  object() : name(), description(), type(objtype_no_type), color(0),
+             hp(0), damage(), dodge(0), defense(0), weight(0), speed(p),
+             attribute(0), value(0), artifact(false), rarity(0)
+  std::string name, description;
+  object_type_t type;
+  dice damage;
+  uint32_t hp, speed, attribute, dodge, defense, weight, attribute value, rarity;
+  bool art_isused, artifact;
+  uint32_t color;
+  object_description &od;
 }
 
 class object_factory{
+public:
+  void gen_dynamic_obj(dungeon *d, object_description *o);
+  void gen_dynamic_mon(dungeon *d, npc *m);
+  void unique_death(dungeon *d, character *m);
 }
 
-void gen_dynamic_obj(dungeon *d, object_description &o);
-void gen_dynamic_mon(dungeon *d, npc *m);
-void unique_death(dungeon *d, character *m);
+// void object_delete(object *o);
+void gen_objects(dungeon *d);
 
 #endif
-
