3bbd2173cfe48edcd0f4ced3f189ec6e24a33116
Haylee Lawrence <hlawrence8121@gmail.com>
2021-04-07 20:50:39 -0500

    Update npc.h

--- 08_loadMonsObj/npc.h
+++ 08_loadMonsObj/npc.h
@@ -5,7 +5,6 @@
 
 # include "dims.h"
 # include "character.h"
-# include "dice.h"
 
 # define NPC_SMART         0x00000001
 # define NPC_TELEPATH      0x00000002
@@ -41,21 +40,21 @@
 # define NPC_BIT31         0x80000000
 
 # define has_characteristic(character, bit)              \
-  ((character)->npc->characteristics & NPC_##bit)
+  (((npc *)character)->npc->characteristics & NPC_##bit)
+# define is_unique(character) has_characteristic(character, UNIQ)
+
+class monster_descripion;
 
 typedef uint32_t npc_characteristics_t;
 
 class npc : public character {
  public:
+  npc(dungeon *d, monster_description &m);
+  ~npc();
   npc_characteristics_t characteristics;
   uint32_t have_seen_pc;
   pair_t pc_last_known_position;
-  
-  // new code!!!!
-  bool is_valid;
-  std::string description;
-  uint32_t abilities, hitpoints, rarity;
-  dice damage;
+  monster_description &md;
 };
 
 void gen_monsters(dungeon *d);
