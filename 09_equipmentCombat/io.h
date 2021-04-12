cd7347836516cdf665f2feceef55d2892a29799f
sanjanaamatya <59930717+sanjanaamatya@users.noreply.github.com>
2021-04-12 13:18:32 -0500

    Update io.h

--- 09_equipmentCombat/io.h
+++ 09_equipmentCombat/io.h
@@ -8,5 +8,10 @@ void io_reset_terminal(void);
 void io_display(dungeon *d);
 void io_handle_input(dungeon *d);
 void io_queue_message(const char *format, ...);
+void io_convertObject(object *o, char *c, uint32_t size);
+void io_wear_item(dungeon *d);
+void io_remove_item(dungeon*d);
+void io_display_inventory(dungeon *d);
+void io_display_equip(dungeon *d);
 
 #endif
