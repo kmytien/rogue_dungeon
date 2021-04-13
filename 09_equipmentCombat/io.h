ef3efbbaaed8c507fc1083e8d685b26083f8aee7
mytien kien! <55749308+kmytien@users.noreply.github.com>
2021-04-12 21:09:07 -0500

    Update io.h

--- 09_equipmentCombat/io.h
+++ 09_equipmentCombat/io.h
@@ -2,6 +2,7 @@
 # define IO_H
 
 class dungeon;
+class object;
 
 void io_init_terminal(void);
 void io_reset_terminal(void);
@@ -9,11 +10,13 @@ void io_display(dungeon *d);
 void io_handle_input(dungeon *d);
 void io_queue_message(const char *format, ...);
 void io_convertObject(object *o, char *c, uint32_t size);
-void io_wear_item(dungeon *d);
+int io_wear_item(dungeon *d);
 void io_remove_item(dungeon*d);
 void io_drop_item(dungeon *d);
 void io_permanent_itemRemoval(dungeon *d);
 void io_display_inventory(dungeon *d);
 void io_display_equip(dungeon *d);
+void io_look_monster(dungeon *d);
+void io_inspect_item(dungeon *d);
 
 #endif
