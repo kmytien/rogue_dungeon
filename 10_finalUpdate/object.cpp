c85e0925e83360fae26c4312b65958cda6bf7476
Haylee Lawrence <hlawrence8121@gmail.com>
2021-04-21 13:58:23 -0500

    potions update

--- 10_finalUpdate/object.cpp
+++ 10_finalUpdate/object.cpp
@@ -4,6 +4,8 @@
 #include "object.h"
 #include "dungeon.h"
 #include "utils.h"
+#include "character.h"
+#include "pc.h"
 
 object::object(object_description &o, pair_t p, object *next) :
   name(o.get_name()),
@@ -36,6 +38,59 @@ object::~object()
   }
 }
 
+//NEW CODE
+void gen_potions(dungeon *d)
+{
+  object *o;
+  uint32_t room;
+  pair_t p;
+  std::vector<object_description> &v = d->object_descriptions;
+  int i;
+
+  do {
+    i = rand_range(0, v.size() - 1);
+  } while ((!v[i].can_be_generated() || !v[i].pass_rarity_roll()) && v[i].get_type() == objtype_POTION);
+  
+  room = rand_range(0, d->num_rooms - 1);
+  do {
+    p[dim_y] = rand_range(d->rooms[room].position[dim_y],
+                          (d->rooms[room].position[dim_y] +
+                           d->rooms[room].size[dim_y] - 1));
+    p[dim_x] = rand_range(d->rooms[room].position[dim_x],
+                          (d->rooms[room].position[dim_x] +
+                           d->rooms[room].size[dim_x] - 1));
+  } while (mappair(p) > ter_stairs);
+
+  o = new object(v[i], p, d->objmap[p[dim_y]][p[dim_x]]);
+
+  d->objmap[p[dim_y]][p[dim_x]] = o;
+  
+}
+
+// NEW CODE
+void drink_potion(dungeon *d, pair_t next) {
+  int h;
+  
+  if(!(d->pc_health)) h = 1000;
+  else h = d->pc_health;
+  
+  std::string name;
+  name = d->objmap[next[dim_y]][next[dim_x]]->get_name();
+
+  // increase health based off of hit from potion
+  if (name == "godly health potion") {
+    if(!(d->pc_health)) d->PC->hp = 1000;
+    if ((d->pc_health)) d->PC->hp = d->pc_health;
+  }
+  else if (name == "spicy health potion") d->PC->hp += h/50;
+  else if (name == "good sized health potion") d->PC->hp += h/75;
+  else d->PC->hp += h/25;
+  
+  // cant go over max
+  if(!(d->pc_health) && d->PC->hp > 1000) d->PC->hp = 1000;
+   if ((d->pc_health) && d->PC->hp > d->pc_health) d->PC->hp = d->pc_health;
+}
+
 void gen_object(dungeon *d)
 {
   object *o;
@@ -66,15 +121,20 @@ void gen_object(dungeon *d)
 
 void gen_objects(dungeon *d)
 {
-  uint32_t i;
+  uint32_t i, p_num;
+  p_num = (rand()%6) + 3;
 
   memset(d->objmap, 0, sizeof (d->objmap));
 
   for (i = 0; i < d->max_objects; i++) {
     gen_object(d);
   }
+  
+  for (i = 0; i < p_num; i++) {
+    gen_potions(d);
+  }
 
-  d->num_objects = d->max_objects;
+  d->num_objects = d->max_objects + p_num;
 }
 
 char object::get_symbol()
